86f40c5ac258d1951e73a4a27b422dfc1125bd94
Mohamed Omran <mohamed_omran72@outlook.com>
2020-04-19 03:26:16 +0200

    feat: add splash

--- app/src/main/java/com/example/thetourguide/ui/activity/HomeActivity.java
+++ app/src/main/java/com/example/thetourguide/ui/activity/HomeActivity.java
@@ -1,16 +1,45 @@
 package com.example.thetourguide.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.thetourguide.R;
+import com.example.thetourguide.ui.fragment.SplashFragment;
+import com.google.android.material.tabs.TabLayout;
 
-public class HomeActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends BaseActivity {
+
+    @BindView(R.id.frame_fragment_container)
+    FrameLayout frameFragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        // add login fragment to author activity
+        SplashFragment splashFragment = new SplashFragment();
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frame_fragment_container, splashFragment);
+        fragmentTransaction.commit();
+
+        // handler delay to splash
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                frameFragmentContainer.setVisibility(View.GONE);
+            }
+        },1500);
     }
 }
--- app/src/main/java/com/example/thetourguide/ui/fragment/SplashFragment.java
+++ app/src/main/java/com/example/thetourguide/ui/fragment/SplashFragment.java
@@ -14,7 +14,7 @@ import com.example.thetourguide.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SplashFragment extends Fragment {
+public class SplashFragment extends BaseFragment {
 
     public SplashFragment() {
         // Required empty public constructor
@@ -25,6 +25,9 @@ public class SplashFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false);
+        View view = inflater.inflate(R.layout.fragment_splash, container, false);
+        initFragment();
+
+        return view;
     }
 }
