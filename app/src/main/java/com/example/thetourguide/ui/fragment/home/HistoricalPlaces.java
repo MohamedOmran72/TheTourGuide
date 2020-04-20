1037a6f5896519dd2f213e4f97e422bbbe0d4291
Mohamed Omran <mohamed_omran72@outlook.com>
2020-04-20 07:17:20 +0200

    feat: add description of historical places

--- app/src/main/java/com/example/thetourguide/ui/fragment/home/HistoricalPlaces.java
+++ app/src/main/java/com/example/thetourguide/ui/fragment/home/HistoricalPlaces.java
@@ -56,19 +56,19 @@ public class HistoricalPlaces extends BaseFragment {
 
             // set data to list
             mItemDataList.add(new ItemData(getString(R.string.historical_places_title_1), getString(R.string.historical_places_address_1),
-                    R.drawable.historical_places_image_1));
+                    getString(R.string.historical_places_description_1), R.drawable.historical_places_image_1));
             mItemDataList.add(new ItemData(getString(R.string.historical_places_title_2), getString(R.string.historical_places_address_2),
-                    R.drawable.historical_places_image_2));
+                    getString(R.string.historical_places_description_2), R.drawable.historical_places_image_2));
             mItemDataList.add(new ItemData(getString(R.string.historical_places_title_3), getString(R.string.historical_places_address_3),
-                    R.drawable.historical_places_image_3));
+                    getString(R.string.historical_places_description_3), R.drawable.historical_places_image_3));
             mItemDataList.add(new ItemData(getString(R.string.historical_places_title_4), getString(R.string.historical_places_address_4),
-                    R.drawable.historical_places_image_4));
+                    getString(R.string.historical_places_description_4), R.drawable.historical_places_image_4));
             mItemDataList.add(new ItemData(getString(R.string.historical_places_title_5), getString(R.string.historical_places_address_5),
-                    R.drawable.historical_places_image_5));
+                    getString(R.string.historical_places_description_5), R.drawable.historical_places_image_5));
             mItemDataList.add(new ItemData(getString(R.string.historical_places_title_6), getString(R.string.historical_places_address_6),
-                    R.drawable.historical_places_image_6));
+                    getString(R.string.historical_places_description_6), R.drawable.historical_places_image_6));
             mItemDataList.add(new ItemData(getString(R.string.historical_places_title_7), getString(R.string.historical_places_address_7),
-                    R.drawable.historical_places_image_7));
+                    getString(R.string.historical_places_description_7), R.drawable.historical_places_image_7));
 
         }
 
