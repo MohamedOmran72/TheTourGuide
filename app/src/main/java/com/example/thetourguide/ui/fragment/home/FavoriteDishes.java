bc0dcb1e55ad742a0d2545450448ba52686b9119
Mohamed Omran <mohamed_omran72@outlook.com>
2020-04-20 07:13:46 +0200

    feat: add description of favorite dishes

--- app/src/main/java/com/example/thetourguide/ui/fragment/home/FavoriteDishes.java
+++ app/src/main/java/com/example/thetourguide/ui/fragment/home/FavoriteDishes.java
@@ -52,19 +52,19 @@ public class FavoriteDishes extends BaseFragment {
 
             // set data to list
             mItemDataList.add(new ItemData(getString(R.string.favorite_dishes_title_1), getString(R.string.favorite_dishes_address_1),
-                    R.drawable.favorite_dishes_image_1));
+                    getString(R.string.favorite_dishes_description_1), R.drawable.favorite_dishes_image_1));
             mItemDataList.add(new ItemData(getString(R.string.favorite_dishes_title_2), getString(R.string.favorite_dishes_address_2),
-                    R.drawable.favorite_dishes_image_2));
+                    getString(R.string.favorite_dishes_description_2), R.drawable.favorite_dishes_image_2));
             mItemDataList.add(new ItemData(getString(R.string.favorite_dishes_title_3), getString(R.string.favorite_dishes_address_3),
-                    R.drawable.favorite_dishes_image_3));
+                    getString(R.string.favorite_dishes_description_3), R.drawable.favorite_dishes_image_3));
             mItemDataList.add(new ItemData(getString(R.string.favorite_dishes_title_4), getString(R.string.favorite_dishes_address_4),
-                    R.drawable.favorite_dishes_image_4));
+                    getString(R.string.favorite_dishes_description_4), R.drawable.favorite_dishes_image_4));
             mItemDataList.add(new ItemData(getString(R.string.favorite_dishes_title_5), getString(R.string.favorite_dishes_address_5),
-                    R.drawable.favorite_dishes_image_5));
+                    getString(R.string.favorite_dishes_description_5), R.drawable.favorite_dishes_image_5));
             mItemDataList.add(new ItemData(getString(R.string.favorite_dishes_title_6), getString(R.string.favorite_dishes_address_6),
-                    R.drawable.favorite_dishes_image_6));
+                    getString(R.string.favorite_dishes_description_6), R.drawable.favorite_dishes_image_6));
             mItemDataList.add(new ItemData(getString(R.string.favorite_dishes_title_7), getString(R.string.favorite_dishes_address_7),
-                    R.drawable.favorite_dishes_image_7));
+                    getString(R.string.favorite_dishes_description_7), R.drawable.favorite_dishes_image_7));
 
         }
 
