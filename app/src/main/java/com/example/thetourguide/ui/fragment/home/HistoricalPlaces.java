e2d902898b3b242aaf19b10cd5838544aa7a4231
Mohamed Omran <mohamed_omran72@outlook.com>
2020-04-20 04:37:04 +0200

    feat: add historical places

--- app/src/main/java/com/example/thetourguide/ui/fragment/home/HistoricalPlaces.java
+++ app/src/main/java/com/example/thetourguide/ui/fragment/home/HistoricalPlaces.java
@@ -29,6 +29,8 @@ public class HistoricalPlaces extends BaseFragment {
     @BindView(R.id.recyclerview_historical_places_fragment)
     RecyclerView recyclerviewHistoricalPlacesFragment;
 
+    private List<ItemData> mItemDataList = new ArrayList<>();
+
     public HistoricalPlaces() {
         // Required empty public constructor
     }
@@ -42,7 +44,36 @@ public class HistoricalPlaces extends BaseFragment {
 
         initFragment();
         ButterKnife.bind(this, view);
-
+        initialize();
         return view;
+    }
+
+    private void initialize() {
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerviewHistoricalPlacesFragment.setLayoutManager(linearLayoutManager);
+        if (mItemDataList.size() == 0) {
+
+            // set data to list
+            mItemDataList.add(new ItemData(getString(R.string.historical_places_title_1), getString(R.string.historical_places_address_1),
+                    R.drawable.historical_places_image_1));
+            mItemDataList.add(new ItemData(getString(R.string.historical_places_title_2), getString(R.string.historical_places_address_2),
+                    R.drawable.historical_places_image_2));
+            mItemDataList.add(new ItemData(getString(R.string.historical_places_title_3), getString(R.string.historical_places_address_3),
+                    R.drawable.historical_places_image_3));
+            mItemDataList.add(new ItemData(getString(R.string.historical_places_title_4), getString(R.string.historical_places_address_4),
+                    R.drawable.historical_places_image_4));
+            mItemDataList.add(new ItemData(getString(R.string.historical_places_title_5), getString(R.string.historical_places_address_5),
+                    R.drawable.historical_places_image_5));
+            mItemDataList.add(new ItemData(getString(R.string.historical_places_title_6), getString(R.string.historical_places_address_6),
+                    R.drawable.historical_places_image_6));
+            mItemDataList.add(new ItemData(getString(R.string.historical_places_title_7), getString(R.string.historical_places_address_7),
+                    R.drawable.historical_places_image_7));
+
+        }
+
+        ItemsAdapter mItemsAdapter = new ItemsAdapter(getActivity(), mItemDataList);
+        recyclerviewHistoricalPlacesFragment.setAdapter(mItemsAdapter);
+
     }
 }
